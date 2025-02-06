e7a78109197e3b3793a473bc41f95527bd3ea6e7
Jay <149704927+jayy0719@users.noreply.github.com>
2025-02-06 13:43:48 -0500

    The introduction to the start of the project with function requirements!!!!

---
+++ code/lights.py
@@ -0,0 +1,36 @@
+import time
+import argparse
+
+'''
+needed:
+    1. # LED_COUNT of LED Pixels
+    2. # LED_PIN of LED Pins
+    3. # LED_FREQ_HZ :Freq of LED in HERTZ
+    4.  LED_DMA ????? -- Research
+    5. LED_BRIGHTNESS of LED 
+    6. LED_INVERT (Signal???) N/A
+    7. LED_CHANNEL ( for GPI0 ->>>>> 13,19,41,45,53) 
+
+
+'''
+
+
+#Functions needed
+
+
+"colorTravel()"
+# Used for displaying led lights as it crosses each pixels
+
+
+"  theaterMode() "
+# Movie Theare chase animations
+
+"Wheel "
+
+"rainbow()"
+
+'theaterChaseRainbow()'
+
+
+if __name__ == '__main__':
+    print('This is the intro to rasplights')
