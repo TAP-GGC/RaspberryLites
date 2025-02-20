5597fdfb5077c5a4db80597b77c151b8447d6be9
Jeron Clarke <jeronclarke@Jerons-MacBook-Air.local>
2025-02-20 13:09:39 -0500

    Added the plan for the end of the project and demonstration.

--- code/temp_code/ temp.py
+++ code/temp_code/ temp.py
@@ -6,7 +6,7 @@ from time import sleep
 # sudo apt install python3-gpiozero
 
 led1 = LED(17)
-button = Button(18)
+
 
 def pulse(led):
     for i in range(0, 101, 5):
@@ -17,10 +17,8 @@ def pulse(led):
         sleep(0.05)
 
 while True:
-    if button.is_pressed:
-        led1.on()
-        pulse(led1)
-        sleep(1)
-    else:
-        led1.off()
-        sleep(0.5)
+
+    led1.on()
+    pulse(led1)
+    sleep(1)
+
